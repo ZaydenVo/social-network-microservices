9634d00ec4cca7343915ca18db9637e635d86637
ZaydenVo <votrantin@gmail.com>
2026-05-18 08:06:33 +0700

    feat(chat): integrate SocketIO broadcasting in ChatMessageService

--- chat-service/src/main/java/com/zayden/chat_service/repository/WebSocketSessionRepository.java
+++ chat-service/src/main/java/com/zayden/chat_service/repository/WebSocketSessionRepository.java
@@ -4,7 +4,11 @@ import com.zayden.chat_service.entity.WebSocketSession;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WebSocketSessionRepository extends MongoRepository<WebSocketSession, String> {
     void deleteBySocketSessionId(String socketId);
+
+    List<WebSocketSession> findAllByUserIdIn(List<String> userIds);
 }
--- chat-service/src/main/java/com/zayden/chat_service/service/ChatMessageService.java
+++ chat-service/src/main/java/com/zayden/chat_service/service/ChatMessageService.java
@@ -1,14 +1,20 @@
 package com.zayden.chat_service.service;
 
+import com.corundumstudio.socketio.SocketIOServer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zayden.chat_service.dto.request.ChatMessageRequest;
 import com.zayden.chat_service.dto.response.ChatMessageResponse;
 import com.zayden.chat_service.entity.ChatMessage;
+import com.zayden.chat_service.entity.ParticipantInfo;
+import com.zayden.chat_service.entity.WebSocketSession;
 import com.zayden.chat_service.exception.AppException;
 import com.zayden.chat_service.exception.ErrorCode;
 import com.zayden.chat_service.mapper.ChatMessageMapper;
 import com.zayden.chat_service.mapper.InfoMapper;
 import com.zayden.chat_service.repository.ChatMessageRepository;
 import com.zayden.chat_service.repository.ConversationRepository;
+import com.zayden.chat_service.repository.WebSocketSessionRepository;
 import com.zayden.chat_service.repository.httpclient.ProfileClient;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +24,10 @@ import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -26,9 +35,12 @@ import java.util.Objects;
 public class ChatMessageService {
     ConversationRepository conversationRepository;
     ChatMessageRepository chatMessageRepository;
+    WebSocketSessionRepository webSocketSessionRepository;
     ProfileClient profileClient;
     InfoMapper infoMapper;
     ChatMessageMapper chatMessageMapper;
+    SocketIOServer socketIOServer;
+    ObjectMapper objectMapper;
 
     public ChatMessageResponse createMessage(ChatMessageRequest request) {
         var userId = SecurityContextHolder.getContext().getAuthentication().getName();
@@ -50,7 +62,30 @@ public class ChatMessageService {
                 .createdDate(Instant.now())
                 .build();
 
-        return toChatMessageResponse(chatMessageRepository.save(chatMessage), userId);
+        ChatMessageResponse chatMessageResponse = chatMessageMapper.toChatMessageResponse(chatMessageRepository.save(chatMessage));
+
+        List<String> userIds = conversation.getParticipants().stream()
+                .map(ParticipantInfo::getUserId)
+                .toList();
+
+        Map<String, WebSocketSession> webSocketSessionMap = webSocketSessionRepository.findAllByUserIdIn(userIds)
+                .stream().collect(Collectors.toMap(WebSocketSession::getSocketSessionId, Function.identity()));
+
+        socketIOServer.getAllClients().forEach(client -> {
+            var webSocketSession = webSocketSessionMap.get(client.getSessionId().toString());
+            if (Objects.nonNull(webSocketSession)) {
+                String message = null;
+                chatMessageResponse.setMe(webSocketSession.getUserId().equals(userId));
+                try {
+                    message = objectMapper.writeValueAsString(chatMessageResponse);
+                    client.sendEvent("message", message);
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        return toChatMessageResponse(chatMessage, userId);
     }
 
     public List<ChatMessageResponse> getChatMessage(String conversationId) {
